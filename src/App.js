3d508f9802cf806a65d6faf963e48d89be1905e1
Prit Shah <shahprit2501@gmail.com>
2021-08-11 22:19:05 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import GiftOpen from './images/giftopen';
+import hp6 from './images/hp6.jpg'
+import hp4 from './images/hp4.jpg'
+import confetti1new from './images/confetti1new.jpg'
+import gift1 from './images/gift1.jpg'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state ={
+    trigger : true
+  }
+  handletrigger = () => {
+    this.setState({trigger:false});
+  }
+  render(){
+    const {trigger} = this.state
+    // console.log(trigger)
+    
+    return (
+      trigger ? 
+      <div className="background">
+        <img src={hp6} className="img1" />
+        <img src={hp4} className="img2" />
+        <img src={confetti1new} className="img3" />
+        <img src={confetti1new} className="img4" />
+        <div className="hb">
+        <h1>Happy Birthday</h1> <h2>To You</h2> <h2>From</h2> <h2>Prit</h2>
+        <img src={gift1} className="gift" onClick={this.handletrigger}/>  
+        </div>
+        
+      </div> : <GiftOpen />
+    );
+    
+  }
 }
 
 export default App;
